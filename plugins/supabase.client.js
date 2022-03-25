3b13fe1173281ec50c9dcc92aa17817ed3e8f15c
Idris Olubisi <heedris2olubisi@gmail.com>
2022-03-26 00:29:04 +0100

    File upload

--- plugins/supabase.client.js
+++ plugins/supabase.client.js
@@ -1,10 +1,6 @@
 /* plugins/client.js */
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
-  "https://lvjrfcwhboqqemflnqre.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2anJmY3doYm9xcWVtZmxucXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDgwNzkxODEsImV4cCI6MTk2MzY1NTE4MX0.VyFiAQWvzH8S-XeRZPfmpONdtNtYaShKHZGl93WX2Co"
-);
-
+const supabase = createClient("https://yoururl.supabase.co", "your-api-key");
 export default (_, inject) => {
   inject("supabase", supabase);
 };
